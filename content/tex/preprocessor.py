37fc735cde8db133c486dacedea0f93fdbb0eb65
brianush1 <brianush1@outlook.com>
2026-02-11 14:28:04 -0500

    put team name in top left

--- content/tex/preprocessor.py
+++ content/tex/preprocessor.py
@@ -260,7 +260,7 @@ def print_header(data, outstream):
         return name if name.startswith('.') else name.split('.')[0]
     output = r"\enspace{}".join(map(adjust, lines[:ind]))
     font_size = 10
-    if header_length > 150:
+    if header_length > 120:
         font_size = 8
     output = r"\hspace{3mm}\textbf{" + output + "}"
     output = "\\fontsize{%d}{%d}" % (font_size, font_size) + output
