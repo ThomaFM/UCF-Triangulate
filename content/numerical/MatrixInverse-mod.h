20e28b9193dc5f5158b7c34a4dd8865f5cd3672f
Thomas Meeks <thomasfm2004@gmail.com>
2026-03-06 19:00:01 -0500

    removed real number matrix inverse, modified prime matrix inverse to be easily modifiable to work with real numbers while maintaining precision.

--- content/numerical/MatrixInverse-mod.h
+++ content/numerical/MatrixInverse-mod.h
@@ -6,6 +6,7 @@
  * Returns rank; result is stored in $A$ unless singular (rank < n).
  * For prime powers, repeatedly set $A^{-1} = A^{-1} (2I - AA^{-1})\  (\text{mod }p^k)$ where $A^{-1}$ starts as
  * the inverse of A mod p, and k is doubled in each step.
+ * modifiable to work with doubles (precisely) by changing commented lines and removing mod
  * Time: O(n^3)
  * Status: Slightly tested
  */
@@ -13,39 +14,37 @@
 
 #include "../number-theory/ModPow.h"
 
-int matInv(vector<vector<ll>>& A) {
+template<class T>
+int matInv(vector<vector<T>>& A) {
 	int n = sz(A); vi col(n);
-	vector<vector<ll>> tmp(n, vector<ll>(n));
+	vector<vector<T>> tmp(n, vector<T>(n));
 	rep(i,0,n) tmp[i][i] = 1, col[i] = i;
-
 	rep(i,0,n) {
 		int r = i, c = i;
-		rep(j,i,n) rep(k,i,n) if (A[j][k]) {
-			r = j; c = k; goto found;
-		}
-		return i;
-found:
+        rep(j,i,n) rep(k,i,n)
+        if(std::abs(A[j][k]) > std::abs(A[r][c]))
+            r = j, c = k;
+        if(std::abs(A[r][c]) <= T(1e-12)) return i;
 		A[i].swap(A[r]); tmp[i].swap(tmp[r]);
 		rep(j,0,n) swap(A[j][i], A[j][c]), swap(tmp[j][i], tmp[j][c]);
 		swap(col[i], col[c]);
-		ll v = modpow(A[i][i], mod - 2);
+		T v = modpow(A[i][i], mod - 2); //T v = T(1)/A[i][i];
 		rep(j,i+1,n) {
-			ll f = A[j][i] * v % mod;
+			T f = A[j][i] * v % mod; 
 			A[j][i] = 0;
-			rep(k,i+1,n) A[j][k] = (A[j][k] - f*A[i][k]) % mod;
-			rep(k,0,n) tmp[j][k] = (tmp[j][k] - f*tmp[i][k]) % mod;
+			rep(k,i+1,n) A[j][k] = (A[j][k] - f*A[i][k])%mod; 
+			rep(k,0,n) tmp[j][k] =(tmp[j][k] - f*tmp[i][k])%mod; 
 		}
-		rep(j,i+1,n) A[i][j] = A[i][j] * v % mod;
+		rep(j,i+1,n) A[i][j] = A[i][j] * v % mod;  
 		rep(j,0,n) tmp[i][j] = tmp[i][j] * v % mod;
 		A[i][i] = 1;
 	}
-
 	for (int i = n-1; i > 0; --i) rep(j,0,i) {
-		ll v = A[j][i];
-		rep(k,0,n) tmp[j][k] = (tmp[j][k] - v*tmp[i][k]) % mod;
+		T v = A[j][i];
+		rep(k,0,n) tmp[j][k] = (tmp[j][k]- v * tmp[i][k])%mod;
 	}
-
 	rep(i,0,n) rep(j,0,n)
-		A[col[i]][col[j]] = tmp[i][j] % mod + (tmp[i][j] < 0 ? mod : 0);
+		A[col[i]][col[j]] = tmp[i][j]
+        % mod + (tmp[i][j] < 0 ? mod : 0); //remove line
 	return n;
 }
