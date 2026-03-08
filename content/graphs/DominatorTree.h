180d4268a2a9c00459a7149e7e2fe094a42e7e4f
Thomas Meeks <thomasfm2004@gmail.com>
2026-03-08 19:18:33 -0400

    change to juicer DomTree

--- content/graphs/DominatorTree.h
+++ content/graphs/DominatorTree.h
@@ -1,54 +1,36 @@
 /**
- * Author: Tyler Marks (based on UCF PT)
- * Description: Builds a dominator tree on a directed graph. Output tree is a parent array with $src$ as the root.
- * Time: O(V+E)
- * Status: Untested
+ * Description: Given a digraph, return the edges of the dominator tree given as an adj. list (directed tree downwards from the root)
+ * Time: $O((n + m) * log n)$ where n is the number of verticies in the graph and m is the number of edges
+ * Status: Tested
  */
-#pragma once
-
-vi getDomTree(vvi &adj, int src) {
-	int n = sz(adj), t = 0;
-	vvi revAdj(n), child(n), sdomChild(n);
-	vi label(n, -1), revLabel(n), sdom(n), idom(n), par(n), best(n);
-
-	auto dfs = [&](int cur, auto &dfs) -> void {
-		label[cur] = t, revLabel[t] = cur;
-		sdom[t] = par[t] = best[t] = t; t++;
-		for(int nxt: adj[cur]) {
-			if(label[nxt] == -1) {
-				dfs(nxt, dfs);
-				child[label[cur]].push_back(label[nxt]);
-			}
-			revAdj[label[nxt]].push_back(label[cur]);
-		}
-	};
-	dfs(src, dfs);
-
-	auto get = [&](int x, auto &get) -> int {
-		if(par[x] != x) {
-			int t = get(par[x], get);
-			par[x] = par[par[x]];
-			if(sdom[t] < sdom[best[x]]) best[x] = t;  
-		}
-		return best[x];
-	};
-
-	for(int i = t-1; i >= 0; i--) {
-		for(int j: revAdj[i]) sdom[i] = min(sdom[i], sdom[get(j, get)]);
-		if(i > 0) sdomChild[sdom[i]].push_back(i);
-		for(int j: sdomChild[i]) {
-			int k = get(j, get);
-			if(sdom[j] == sdom[k]) idom[j] = sdom[j];
-			else idom[j] = k;
-		}
-		for(int j: child[i]) par[j] = i;
-	}
-
-	vi dom(n);
-	rep(i, 1, t) {
-		if(idom[i] != sdom[i]) idom[i] = idom[idom[i]];
-		dom[revLabel[i]] = revLabel[idom[i]];
-	}
-
-	return dom;
-}
+vector<vi> dominator_tree(const vector<vi>& adj, int root) {
+	int n = sz(adj) + 1, co = 0;
+	vector<vi> ans(n), radj(n), child(n), sdomChild(n);
+	vi label(n), rlabel(n), sdom(n), dom(n), par(n), bes(n);
+	auto get = [&](auto self, int x) -> int {
+	  if (par[x] != x) {
+		int t = self(self, par[x]);
+		par[x] = par[par[x]];
+		if (sdom[t] < sdom[bes[x]]) bes[x] = t; }
+	  return bes[x]; };
+	auto dfs = [&](auto self, int x) -> void {
+	  label[x] = ++co, rlabel[co] = x;
+	  sdom[co] = par[co] = bes[co] = co;
+	  for (auto y : adj[x]) {
+		if (!label[y])
+		  self(self, y), child[label[x]].push_back(label[y]);
+		radj[label[y]].push_back(label[x]); } };
+	dfs(dfs, root);
+	for (int i = co; i >= 1; --i) {
+	  for (auto j : radj[i])
+		sdom[i] = min(sdom[i], sdom[get(get, j)]);
+	  if (i > 1) sdomChild[sdom[i]].push_back(i);
+	  for (auto j : sdomChild[i]) {
+		int k = get(get, j);
+		if (sdom[j] == sdom[k]) dom[j] = sdom[j];
+		else dom[j] = k; }
+	  for (auto j : child[i]) par[j] = i; }
+	for (int i = 2; i < co + 1; ++i) {
+	  if (dom[i] != sdom[i]) dom[i] = dom[dom[i]];
+	  ans[rlabel[dom[i]]].push_back(rlabel[i]); }
+	return ans; }
